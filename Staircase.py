6a6227fe2e51112eadfc3ceafe197785a5f9850d
Mlle NGUYEN <38267385+minhnnguyen29@users.noreply.github.com>
2020-03-22 10:34:43 +1100

    Create Staircase.py

---
+++ Staircase.py
@@ -0,0 +1,20 @@
+"""
+Problem: https://www.hackerrank.com/challenges/staircase/problem
+"""
+
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the staircase function below.
+def staircase(n):
+    for i in range(n): 
+        print(" "*(n-i-1)+"#"*(i+1))
+if __name__ == '__main__':
+    n = int(raw_input())
+
+    staircase(n)
